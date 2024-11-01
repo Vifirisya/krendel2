874c13945fb17ea76e9d4c2d13ab2bc0698a1f2e
Vifirisya <timstov07@gmail.com>
2024-11-01 18:46:50 +0300

    nowow

--- launch/launcher/twistPublisher.py
+++ launch/launcher/twistPublisher.py
@@ -25,8 +25,8 @@ class TwistPublisher(Node):
         #self.listenThread.start()
         self.listenThread = Thread(target=self.listen)
         self.listenThread.start()
-        #self.publisher_ = self.create_publisher(Twist, "diff_drive_controller/cmd_vel_unstamped", 10)
-        self.publisher_ = self.create_publisher(Twist, "/wow", 10)
+        self.publisher_ = self.create_publisher(Twist, "diff_drive_controller/cmd_vel_unstamped", 10)
+        #self.publisher_ = self.create_publisher(Twist, "/wow", 10)
         self.timer_ = self.create_timer(1.0/5.0, self.publish)
         self.cmd_value = (123.0, 321.0)
 
