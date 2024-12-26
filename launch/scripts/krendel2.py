0211fce9ebc871b0a193c332b9cd9ee5406d5bd4
Vifirisya <timstov07@gmail.com>
2024-12-26 13:59:59 +0300

    return to nonsense

--- launch/scripts/krendel2.py
+++ launch/scripts/krendel2.py
@@ -20,53 +20,53 @@ def go(pointName):
 
     p = points.readPoints()
     pos = p[pointName]
-    #os.system(f"ros2 topic pub /goal_pose geometry_msgs/PoseStamped \"{{header: {{stamp: {{sec: 0}}, frame_id: \'map\'}}, pose: {{position: {{x: {pos[0]}, y: {pos[1]}, z: 0.0}}, orientation: {{w: 1.0}}}}}}\"")
+    os.system(f"ros2 topic pub /goal_pose geometry_msgs/PoseStamped \"{{header: {{stamp: {{sec: 0}}, frame_id: \'map\'}}, pose: {{position: {{x: {pos[0]}, y: {pos[1]}, z: 0.0}}, orientation: {{w: 1.0}}}}}}\"")
 
-    goal_pose = PoseStamped()
-    goal_pose.header.frame_id = 'map'
-    goal_pose.header.stamp = navigator.get_clock().now().to_msg()
-    goal_pose.pose.position.x = pos[0]
-    goal_pose.pose.position.y = pos[1]
-    goal_pose.pose.position.z = 0.0
-    goal_pose.pose.orientation.x = 0.0
-    goal_pose.pose.orientation.y = 0.0
-    goal_pose.pose.orientation.z = 0.0
-    goal_pose.pose.orientation.w = 1.0
+    # goal_pose = PoseStamped()
+    # goal_pose.header.frame_id = 'map'
+    # goal_pose.header.stamp = navigator.get_clock().now().to_msg()
+    # goal_pose.pose.position.x = pos[0]
+    # goal_pose.pose.position.y = pos[1]
+    # goal_pose.pose.position.z = 0.0
+    # goal_pose.pose.orientation.x = 0.0
+    # goal_pose.pose.orientation.y = 0.0
+    # goal_pose.pose.orientation.z = 0.0
+    # goal_pose.pose.orientation.w = 1.0
 
-    navigator.goToPose(goal_pose)
+    # navigator.goToPose(goal_pose)
 
-    i = 0
+    # i = 0
  
-    # Keep doing stuff as long as the robot is moving towards the goal
-    while not navigator.isNavComplete():
-        ################################################
-        #
-        # Implement some code here for your application!
-        #
-        ################################################
+    # # Keep doing stuff as long as the robot is moving towards the goal
+    # while not navigator.isNavComplete():
+    #     ################################################
+    #     #
+    #     # Implement some code here for your application!
+    #     #
+    #     ################################################
     
-        # Do something with the feedback
-        i = i + 1
-        feedback = navigator.getFeedback()
-        if feedback and i % 5 == 0:
-            print('Distance remaining: ' + '{:.2f}'.format(feedback.distance_remaining) + ' meters.')
+    #     # Do something with the feedback
+    #     i = i + 1
+    #     feedback = navigator.getFeedback()
+    #     if feedback and i % 5 == 0:
+    #         print('Distance remaining: ' + '{:.2f}'.format(feedback.distance_remaining) + ' meters.')
     
-        # Some navigation timeout to demo cancellation
-        if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
-            navigator.cancelNav()
+    #     # Some navigation timeout to demo cancellation
+    #     if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+    #         navigator.cancelNav()
     
-        # Some navigation request change to demo preemption
-        if Duration.from_msg(feedback.navigation_time) > Duration(seconds=120.0):
-            goal_pose.pose.position.x = -3.0
-            navigator.goToPose(goal_pose)
+    #     # Some navigation request change to demo preemption
+    #     if Duration.from_msg(feedback.navigation_time) > Duration(seconds=120.0):
+    #         goal_pose.pose.position.x = -3.0
+    #         navigator.goToPose(goal_pose)
     
-    # Do something depending on the return code
-    result = navigator.getResult()
-    if result == NavigationResult.SUCCEEDED:
-        print('\nGoal succeeded!\n')
-    elif result == NavigationResult.CANCELED:
-        print('\nGoal was canceled!\n')
-    elif result == NavigationResult.FAILED:
-        print('\nGoal failed!\n')
-    else:
-        print('\nGoal has an invalid return status!\n')
+    # # Do something depending on the return code
+    # result = navigator.getResult()
+    # if result == NavigationResult.SUCCEEDED:
+    #     print('\nGoal succeeded!\n')
+    # elif result == NavigationResult.CANCELED:
+    #     print('\nGoal was canceled!\n')
+    # elif result == NavigationResult.FAILED:
+    #     print('\nGoal failed!\n')
+    # else:
+    #     print('\nGoal has an invalid return status!\n')
