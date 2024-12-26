b9d24e12eeaaeec9c68ab1d28f258ac8dce62066
Vifirisya <timstov07@gmail.com>
2024-12-26 13:30:39 +0300

    fix init

--- launch/scripts/krendel2.py
+++ launch/scripts/krendel2.py
@@ -9,8 +9,8 @@ sys.path.insert(0, os.path.realpath(__file__).replace(f"scripts/krendel2.py", "l
 import points
 from robot_navigator import *
 
-navigator = BasicNavigator()
 rclpy.init()
+navigator = BasicNavigator()
 
 navigator.waitUntilNav2Active()
 navigator.changeMap(os.path.realpath(__file__).replace(f"scripts/krendel2.py", "launcher/map.yaml"))
