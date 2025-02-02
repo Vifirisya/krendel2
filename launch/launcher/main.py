b5735c08623524c073f2941cf4745811e7c0c37c
Vifirisya <timstov07@gmail.com>
2025-02-02 23:06:36 +0300

    yo

--- launch/launcher/main.py
+++ launch/launcher/main.py
@@ -108,8 +108,8 @@ def overseer(T):
             message = "s:" + letter + ':' + str(int(status))
             communication.simplySend(message, (IP, 2002))
         if launcher.processes['navigation'].process != None:
-            communication.simplySend(launcher.processes['navigation'].readLine(), (IP, 2004))
-
+            #communication.simplySend(launcher.processes['navigation'].readLine(), (IP, 2004))
+            communication.simplySend("yo", (IP, 2004))
             #print(process, status)
         #print("\n")
 
