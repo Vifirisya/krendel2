50ebc9b83b81dd7f9f5bf57ed3898d9bdb3ddf10
Vifirisya <timstov07@gmail.com>
2025-02-04 13:08:37 +0300

    next test

--- launch/launcher/launcher.py
+++ launch/launcher/launcher.py
@@ -32,7 +32,7 @@ class Process:
 
     def readLine(self):
         try:
-            return self.process.stdout.readline
+            return next(iter(self.process.stdout.readline))
         finally: 
             return ""
 
