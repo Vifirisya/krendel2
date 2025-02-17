33d72e710c3eb1e7343754d32c984fbb82856978
Vifirisya <timstov07@gmail.com>
2025-02-17 12:01:39 +0300

    : >

--- launch/launcher/main.py
+++ launch/launcher/main.py
@@ -82,7 +82,7 @@ def eraseMap(data=None):
         os.remove(os.path.realpath(__file__).replace(f"/main.py", "/map.posegraph"))
         print("The map is erased")
     except FileNotFoundError:
-        print>("The map has already been erased")
+        print("The map has already been erased")
 
 worklist = {'l':launch,
             's':speed,
